3aa816fe3dbabb42ecbf53722882aa7e43a8080f
Motoko TANAKA <M.TANAKA@f5.com>
2021-02-10 21:34:45 +0900

    add copy button

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('sphinx_copybutton'))
 
 
 # -- Project information -----------------------------------------------------
